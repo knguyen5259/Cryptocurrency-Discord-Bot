1a4a6c0a8b60c7556415183fc95af6ca6b88953b
knguyen5259 <91755924+knguyen5259@users.noreply.github.com>
2021-10-15 18:45:08 -0500

    Add files via upload

---
+++ bot/__init__.py
@@ -0,0 +1,3 @@
+__author__ = "knguyen5259"
+__version__ = "v0.0.1"
+__license__ = "MIT"
---
+++ bot/__main__.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from json import loads, dumps
+from lightbulb import Bot
+
+from bot.commands.crypto import Crypto
+from bot.commands.misc import Misc
+
+
+def main() -> None:
+    if not Path("config.json").exists():
+        with open("config.json", "w", encoding="utf-8") as file:
+            file.write(dumps({
+                "token": "BOT TOKEN",
+                "prefix": "BOT PREFIX",
+                "rate_limit": 30  # seconds
+            }))
+
+        return
+
+    with open("config.json", "r", encoding="utf-8") as file:
+        config = loads(file.read())
+
+    bot = Bot(
+        token=config["token"],
+        prefix=config["prefix"]
+    )
+
+    bot.add_plugin(Crypto(config["rate_limit"]))
+    bot.add_plugin(Misc())
+
+    bot.run()
+
+
+if __name__ == "__main__":
+    main()
