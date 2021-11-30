4f1c8a562dc41d708828a46a150f3ca76a00b5ef
knguyen5259 <91755924+knguyen5259@users.noreply.github.com>
2021-11-30 14:52:40 -0600

    Added embeds

--- bot/commands/crypto.py
+++ bot/commands/crypto.py
@@ -1,5 +1,7 @@
 from lightbulb import Plugin, command
 from lightbulb.context import Context
+from hikari.embeds import Embed
+from hikari import Color
 
 
 class Crypto(Plugin):
@@ -11,15 +13,15 @@ class Crypto(Plugin):
         coin_price = await self.bot.markets.get_coin(coin)
 
         if coin_price is not None:
-            await ctx.respond(f"Price: ${coin_price}")
+            await ctx.respond(embed=Embed(title="Price", description=f"${coin_price}", color=Color.from_rgb(0, 0, 200)))
         else:
-            await ctx.respond("Sorry, coin not found.")
+            await ctx.respond(embed=Embed(title="Error", description=f"Coin not found", color=Color.from_rgb(200, 0, 0)))
 
     @command("change", "Gets the 24 hour change of a cryptocurrency")
     async def change(self, ctx: Context, coin: str) -> None:
         coin_change = await self.bot.markets.get_day_change(coin)
 
         if coin_change is not None:
-            await ctx.respond(f"Change: ${coin_change}")
+            await ctx.respond(embed=Embed(title="Change", description=f"${coin_change}", color=Color.from_rgb(0, 0, 200)))
         else:
-            await ctx.respond("Sorry, coin not found.")
+            await ctx.respond(embed=Embed(title="Error", description=f"Coin not found", color=Color.from_rgb(200, 0, 0)))
